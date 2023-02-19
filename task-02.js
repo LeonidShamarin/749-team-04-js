ead47b54a7dc5f66815f79eec9949f36d243c22a
KaterinaLukyanenko <118893094+KaterinaLukyanenko@users.noreply.github.com>
2023-02-20 01:09:13 +0300

    Update task-02.js
    add task 7

--- task-02.js
+++ task-02.js
@@ -69,3 +69,13 @@ function warnTheSheep(queue) {
   );
 }
 
+//https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
+function firstNonConsecutive(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] > 1) {
+      return arr[i];
+    }
+  }
+
+  return null;
+}
