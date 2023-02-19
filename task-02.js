85b1460c711df3fc5203cb9e5c1804f7093fe0af
KaterinaLukyanenko <118893094+KaterinaLukyanenko@users.noreply.github.com>
2023-02-19 23:38:49 +0300

    Update task-02.js
    change task1 and task2

--- task-02.js
+++ task-02.js
@@ -1,15 +1,7 @@
 //https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-stringToArray = function (str) {
-    return str.trim().split(" ");
-    };
-  console.log(stringToArray("Robin Singh"));
 
-
-  //Yatsun__________________
   function stringToArray(string){
-
-    return string.split(" ");
-  
+    return string.split(" ");  
   }
 
   //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
@@ -19,47 +11,31 @@ stringToArray = function (str) {
 
  // https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
- function max(numbers) {
-    let maxNum = numbers[0];
-    for (let i = 1; i < numbers.length; i++) {
-      if (numbers[i] > maxNum) {
-        maxNum = numbers[i];
-      }
-    } 
-    return maxNum;
-  }
-  function min(numbers) {
-    let minNum = numbers[0];
-    for (let i = 1; i < numbers.length; i++) {
-      if (numbers[i] < minNum) {
-        minNum = numbers[i];
-      }
-    }
-    
-    return minNum;
-  }
+ var min = function (list) {
+  list.sort((a, b) => a - b);
+  return list[0];
+};
+
+var max = function (list) {
+  list.sort((a, b) => b - a);
+  return list[0];
+};
   
-  //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
-  function min(arr, toReturn) {
-    let smallestValue = arr[0];
-    let index = 0;
-    
-    for(let i=0; i<arr.length; i++){
-      if(arr[i]<smallestValue){
-        smallestValue = arr[i];
-        index = i;
-      }
-    }
-    
-    if(toReturn === "value"){
-      return smallestValue;
-    } else if( toReturn === "index"){
-      return index;
+function min(arr, toReturn) {
+  let min = arr[0];
+  let pos = 0;
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+      pos = i;
     }
   }
+  return toReturn === 'value' ? min : pos;
+}
 
-  https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+//https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
 
   function warnTheSheep(queue) {
     let indexOfWolf = queue.indexOf("wolf");
