88cc1ab5e8282239ebeb443dbab4c70206228ad2
KaterinaLukyanenko <118893094+KaterinaLukyanenko@users.noreply.github.com>
2023-02-20 00:45:23 +0300

    Update task-02.js
    add fifth task

--- task-02.js
+++ task-02.js
@@ -56,5 +56,16 @@ const min = (arr, toReturn, minArr = Math.min(...arr)) =>
     }
   }
   
-
+//Інший варіант
+function warnTheSheep(queue) {
+  if (queue[queue.length - 1] === "wolf") {
+    return "Pls go away and stop eating my sheep";
+  }
+  queue.reverse();
+  return (
+    "Oi! Sheep number " +
+    queue.indexOf("wolf") +
+    "! You are about to be eaten by a wolf!"
+  );
+}
 
