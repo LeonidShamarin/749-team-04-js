85e66dbe084e6a1f7e29080c4201e1fa8f06059a
LeonidShamarin <122298176+LeonidShamarin@users.noreply.github.com>
2023-02-22 00:25:50 +0200

    zadacha3

--- task-03.js
+++ task-03.js
@@ -9,4 +9,20 @@ function findSmallestInteger(arr) {
     const circumference = 2 * Math.PI * circle.radius;
     return parseFloat(circumference.toFixed(6));
   }
+
+
+
+  //https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+  const giveMeFive = object => {
+    const result = []
+  
+    for (const key in object) {
+      if (object.hasOwnProperty(key)) {
+        if (key.length === 5) result.push(key)
+        if (object[key].length === 5) result.push(object[key])
+      }
+    }
+  
+    return result
+  }
   
