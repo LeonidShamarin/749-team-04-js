e81850d71c254494e5ac71252c568f8f3e4eed3f
LeonidShamarin <122298176+LeonidShamarin@users.noreply.github.com>
2023-02-22 00:14:54 +0200

    zadachi1 and 2

--- task-03.js
+++ task-03.js
@@ -1,1 +1,12 @@
-
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+function findSmallestInteger(arr) {
+    return Math.min(...arr);
+  }
+  
+  
+  //https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+  function circleCircumference(circle) {
+    const circumference = 2 * Math.PI * circle.radius;
+    return parseFloat(circumference.toFixed(6));
+  }
+  
