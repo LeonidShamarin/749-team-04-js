bacc45c0a20466fb77d3e495b51723c929d83830
yatsun7 <119352847+yatsun7@users.noreply.github.com>
2023-02-22 13:17:38 +0000

    add task

--- task-03.js
+++ task-03.js
@@ -26,3 +26,35 @@ const giveMeFive = object => {
   return result
 }
 
+//https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, status);
+    this.legs = 0;
+    this.species = "shark";
+  }
+}
+
+class Cat extends Animal {
+    constructor(name, age, status) {
+    super(name, age, status);
+    this.legs = 4;
+    this.species = "cat";
+  }
+    introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;} 
+  
+}
+
+class Dog extends Animal {
+    constructor(name, age, status, master) {
+    super(name, age, status);
+    this.master = master;
+    this.legs = 4;
+    this.species = "dog";
+  }
+    greetMaster(){
+    return `Hello ${this.master}`;
+    } 
+}
